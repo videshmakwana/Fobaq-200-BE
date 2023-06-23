ef758da0bb0176cee9f2d7b566857cfd6da943cc
Videsh Makwana <videsh.m@brilworks.com>
2023-06-23 16:04:42 +0530

    push

---
+++ Fobaq-200/src/main/java/fobaq/com/Fobaq200Application.java
@@ -0,0 +1,13 @@
+package fobaq.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fobaq200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fobaq200Application.class, args);
+	}
+
+}
---
+++ Fobaq-200/src/test/java/fobaq/com/Fobaq200ApplicationTests.java
@@ -0,0 +1,13 @@
+package fobaq.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fobaq200ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
