376c9fc8fdabfbf82b2412e1fab6cd9431e4c8bd
Videsh Makwana <videsh.m@brilworks.com>
2023-06-23 18:25:55 +0530

    say hello

--- Fobaq-200/src/main/java/controllers/TestController.java
+++ Fobaq-200/src/main/java/controllers/TestController.java
@@ -1,4 +1,17 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+@CrossOrigin(origins = "*")
 public class TestController {
+    @GetMapping("/msg")
+    public String getMessa()
+    {
+        return "Hello";
+    }
 }
